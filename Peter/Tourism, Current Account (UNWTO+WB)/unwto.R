d7cb0e6c6ceafda52e7523624d4de31e809f0916
peterc210 <peterc@reed.edu>
2025-08-07 16:16:00 -0500

    fixing file names

--- Peter/Tourism, Current Account (UNWTO+WB)/unwto.R
+++ Peter/Tourism, Current Account (UNWTO+WB)/unwto.R
@@ -23,9 +23,11 @@ export_share <- export_share %>%
 #write.xlsx(export_share, "Average Export Share for Cases (1995-2022).xlsx")
 
 #inbound tourism over current account credits
-tourism_current_account <- read_excel("UNWTO_SIDS complementary_indicators.xlsx", 
+#tourism_current_account <- read_excel("UNWTO_SIDS complementary_indicators.xlsx", 
                                                   sheet = "current account prop", skip = 1)
 #write.xlsx(tourism_current_account, "Inbound Tourism Over Current Account Credits (1995-2022).xlsx")
+#tourism_current_account <- read_excel("Inbound Tourism Over Current Account Credits (1995-2022).xlsx")
+
 
 current_account_share <- tourism_current_account %>% 
   filter(COUNTRY %in% c("Maldives", "Mauritius", "Dominican Republic", "Fiji", "Jamaica"))
@@ -90,6 +92,13 @@ conc_2$Year <- str_remove(conc_2$Year, "_Concentration_Index_Value")
 rooms_current_account <- left_join(rooms_current_account, conc_2)
 
 write.xlsx(rooms_current_account, "current_account_tourism_regressions.xlsx")
+rooms_current_account <- read_excel("current_account_tourism_regressions.xlsx")
+
+rooms_current_account <- rooms_current_account %>% 
+  mutate(delta_rooms = rooms-lag(rooms)) %>% 
+  mutate(rooms = (delta_rooms/lag(rooms)*100)) %>% 
+  select(-delta_rooms)
+  
 
 #linear regression
 results <- rooms_current_account %>% 
@@ -111,5 +120,42 @@ ggplot(data = rm_sing, aes(x = rooms, y = ca_balance))+
   geom_point()+
   geom_smooth(method = "lm", se = F)+
   facet_wrap(~Country)
+#furhter graphing
+ggplot(data = rooms_current_account, aes(x = tourism_over_ca_credit, y = rooms))+
+  geom_point()+
+  geom_smooth(method = "lm", se = F)+
+  facet_wrap(~Country)+
+  xlab("Inbound Tourism Expenditure over Current Account Credits")+
+  ylab("Annual Percentage Change in Number of Hotel Rooms")
+
+plot_tourism_vs_rooms(rooms_current_account, c("Barbados", "Belize", "Cabo Verde", "Maldives", "Fiji", "Mauritius", 
+                                               "Comoros", "Dominica", "Dominican Republic", "Fiji", "Grenada", "Guyana", "Jamaica", 
+                                               "Kiribati", "Maldvies", 
+                                               "Mauritius", "Palau", "Signapore", "Solomon Islands", "Timor-Leste", "Seychelles"))
+
+
+plot_tourism_vs_rooms <- function(data, countries, width = 7, height = 5) {
+  # Loop through each country
+  for (country in countries) {
+    
+    # Filter for one country
+    country_data <- data %>% 
+      filter(Country == country)
+    
+    # Create plot
+    p <- ggplot(country_data, aes(x = tourism_over_ca_credit, y = rooms)) +
+      geom_point() +
+      geom_smooth(method = "lm", se = FALSE) +
+      xlab("Inbound Tourism Expenditure over Current Account Credits") +
+      ylab("Annual Percentage Change in Number of Hotel Rooms") +
+      ggtitle(paste("Tourism vs Rooms in", country))
+    
+    # Define filename
+    file_name <- paste0("tourism_vs_rooms_", gsub(" ", "_", country), ".png")
+    
+    # Save plot
+    ggsave(filename = file_name, plot = p, width = width, height = height)
+  }
+}
 
   
